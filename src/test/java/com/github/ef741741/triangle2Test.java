9266eda264b567594cc3e47ccb6e980ad5e854dc
ef741741 <ef741741@gmail.com>
2017-12-07 20:04:10 +0800

    first

---
+++ src/test/java/com/github/ef741741/triangle2Test.java
@@ -0,0 +1,19 @@
+package com.github.ef741741;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class triangle2Test {
+    @Test
+    public void getType() throws Exception {
+        triangle triang = new triangle(5, 10, 5);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("不是三角形", result);
+    }
+
+}

b11ce9da12ae4179067f94d3a9a7826f59cfbc02
ef741741 <ef741741@gmail.com>
2017-12-07 20:13:26 +0800

    first1

---
+++ src/test/java/com/github/ef741741/TriangleTest.java
@@ -0,0 +1,12 @@
+package com.github.ef741741;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+    }
+
+}
